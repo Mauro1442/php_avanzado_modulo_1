b052c1746565a8b3a1e7e20b52a0623731d7f99d
maurodirosa <maurodirosa19@gmail.com>
2023-07-26 22:29:21 +0200

    add home temp content.

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,11 +1,9 @@
 <h1 class = "pageTitle">Welcome Username!</h1>
-    <section>
 
     <p>This page will soon let you register, login and make CRUD operations on the SQL database with the use of the 
-        Model View Controller (MVC). Hopefully we will also build api routes to make the same operations through 
-    a different front end applicaion. We could even display your real name in the header! 
+        Model View Controller (MVC). <br> Hopefully we will also build api routes to make the same operations through 
+    a different front end applicaion. We could even display your real name in the header! <br>
     See you here in one month when the course resumes. Have a nice day Username! </p>
-    </section>
-</form>
+
     </body>
     </html>
---
+++ views/pages/logout.php
@@ -0,0 +1,4 @@
+<h1 class = "pageTitle">See you soon!</h1>
+
+    </body>
+    </html>
